4b0d99617517b5ba02908a8e1bc11353a666e071
Andreas Lohr <andr3as.lohr@gmail.com>
2014-06-26 00:17:54 +0200

    added NAMESPACE

--- R/percs.R
+++ R/percs.R
@@ -1,17 +1,37 @@
+#' A one sentence description of what your function does
+#'
+#' A more detailed description of what the function is and how
+#' it works. It may be a paragraph that should not be separated
+#' by any spaces.
+#'
+#' @param inputParameter1 A description of the input parameter \code{inputParameter1}
+#' @param inputParameter2 A description of the input parameter \code{inputParameter2}
+#'
+#' @return output A description of the object the function outputs
+#'
+#' @keywords keywords
+#'
+#' @export
+#'
+#' @examples
+#
+#' set.seed(0)
+#' round(percs(rnorm(1000)), 2)
+
 percs <- function(x, na.rm = TRUE, cols = NULL) {
-  
+
   valid <- function(x) { sum(!is.na(x)) }
-  
+
   if (!na.rm) {
     x <- na.omit(x)
   }
 
   percentiles <- quantile(x, c(.68, .95, .997, .999), na.rm = na.rm)
-  
+
   # save column names for later
   names <- names(percentiles)
   attributes(percentiles) <- NULL
-  
+
   dataframe <- data.frame(
     n = valid(x),
     mean = mean(x, na.rm = na.rm),
@@ -24,10 +44,10 @@ percs <- function(x, na.rm = TRUE, cols = NULL) {
     p4 = percentiles[4],
     max = max(x, na.rm = na.rm)
   )
-  
+
   # fix the percentile column names
   p <- match("p1", names(dataframe))
   colnames(dataframe)[p:(p+3)] <- names
-  
+
   dataframe
 }
