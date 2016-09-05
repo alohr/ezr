33dc50d8569eb353ceb477c1f90e51873f5e63ef
Andreas Lohr <andreas.lohr@deutsche-boerse.com>
2016-09-05 09:33:15 +0200

    added more percentiles

--- R/describe.R
+++ R/describe.R
@@ -22,11 +22,11 @@ describe <- function(x, na.rm = TRUE, cols = NULL) {
 
   valid <- function(x) { sum(!is.na(x)) }
 
-  if (!na.rm) {
-    x <- na.omit(x)
-  }
+  #if (!na.rm) {
+  #  x <- na.omit(x)
+  #}
 
-  percentiles <- quantile(x, c(.68, .95, .997, .999), na.rm = na.rm)
+  percentiles <- quantile(x, c(0.001, 0.05, .5, .68, .95, .997, .999), na.rm = na.rm)
 
   # save column names for later
   names <- names(percentiles)
@@ -37,17 +37,19 @@ describe <- function(x, na.rm = TRUE, cols = NULL) {
     mean = mean(x, na.rm = na.rm),
     sd = sd(x, na.rm = na.rm),
     min = min(x, na.rm = na.rm),
-    median = median(x, na.rm = na.rm),
     p1 = percentiles[1],
     p2 = percentiles[2],
     p3 = percentiles[3],
     p4 = percentiles[4],
+    p5 = percentiles[5],
+    p6 = percentiles[6],
+    p7 = percentiles[7],
     max = max(x, na.rm = na.rm)
   )
 
   # fix the percentile column names
   p <- match("p1", names(dataframe))
-  colnames(dataframe)[p:(p+3)] <- names
+  colnames(dataframe)[p:(p+6)] <- names
 
   dataframe
 }
